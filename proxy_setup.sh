2132862fd2292980199e105d56c9606aeafc5f5f
Chris Morgan <mihalis68@gmail.com>
2014-06-20 07:22:30 -0400

    add a sanity check to proxy setup, correct sample IP

--- proxy_setup.sh
+++ proxy_setup.sh
@@ -3,19 +3,27 @@
 # Make sure this file defines CURL in any case
 # Only define http_proxy if you will be using a proxy
 #
-
-# sample setup using a local squid cache at 10.0.1.2 - the hypervisor
-# change to reflect your real proxy info
-#export PROXY="10.0.1.2:3128"
+# Sample setup using a local squid cache at 10.0.100.2 - the
+# hypervisor. Change to reflect your real proxy info
+#export PROXY="10.0.100.2:3128"
 
 export CURL='curl'
 if [ -n "$PROXY" ]; then
-  echo "Using a proxy at $PROXY"
-  
-  export http_proxy=http://${PROXY}
-  export https_proxy=https://${PROXY}
-  
-  # to ignore SSL errors
-  export GIT_SSL_NO_VERIFY=true
-  export CURL="curl -k -x http://${PROXY}"
+    
+   echo "Setting up and testing proxy"
+   
+   export http_proxy=http://${PROXY}
+   export https_proxy=https://${PROXY}
+   
+   # to ignore SSL errors
+   export GIT_SSL_NO_VERIFY=true
+   export CURL="curl -k -x http://${PROXY}"
+   
+   # sanity check
+   $CURL -s --connect-timeout 1 http://www.ubuntu.com > /dev/null
+   if [[ $? != 0 ]]; then
+       echo "Error: proxy $PROXY not responding"
+   else
+       echo "Using a proxy at $PROXY"
+   fi   
 fi
