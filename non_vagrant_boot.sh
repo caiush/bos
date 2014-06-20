5030d2cd060c773f6f08d1bb7857c5d94be460dc
Chris Morgan <mihalis68@gmail.com>
2014-06-20 07:07:38 -0400

    allow specifying the IP address of the bootstrap node

--- non_vagrant_boot.sh
+++ non_vagrant_boot.sh
@@ -22,12 +22,12 @@ fi
 ./vm-to-cluster.sh bloomberg.com
 while read HOSTNAME MACADDR IPADDR ILOIPADDR DOMAIN ROLE; do
     if [[ $HOSTNAME = "end" ]]; then
-	break;
+        break;
     fi
     if [[ "$HOSTNAME" = bcpc-bootstrap ]]; then
-	NATADDR=$IPADDR
-	echo "bcpc-bootstrap is currently at (NAT) address $NATADDR"
-	break
+        NATADDR=$IPADDR
+        echo "bcpc-bootstrap is currently at (NAT) address $NATADDR"
+        break
     fi
 done < cluster.txt
 if [[ -z $NATADDR ]]; then
@@ -35,39 +35,41 @@ if [[ -z $NATADDR ]]; then
     exit 1
 else 
     for ATTEMPT in A B C D E F G; do
-	UP=`fping -aq "$NATADDR" | awk '{print $1}'`
-	if [[ -z "$UP" ]]; then
-	    echo "VM is not up yet or is unreachable"
-	    if [[ $ATTEMPT = G ]]; then
-		echo "Maybe machine needs rework. Rerun this when it's up"
-		exit 1
-	    fi
-	    sleep 5
-	else
-	    break
-	fi
+        if [[ "$1" ]]; then
+            HOST="$1"
+        else
+            HOST="$NATADDR"
+        fi
+        UP=`fping -aq "$HOST" | awk '{print $1}'`
+        if [[ -z "$UP" ]]; then
+            echo "VM is not up yet or is unreachable"
+            if [[ $ATTEMPT = G ]]; then
+                echo "Maybe machine needs rework. Rerun this when it's up"
+                exit 1
+            fi
+            sleep 5
+        else
+            break
+        fi
     done
     SSHCOMMON="-q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o VerifyHostKeyDNS=no"
-#    SSHCOMMON=" -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o VerifyHostKeyDNS=no"
     SSHCMD="ssh $SSHCOMMON"
     SCPCMD="scp $SSHCOMMON"
-    EDITED=`sshpass -p ubuntu $SSHCMD -t ubuntu@$NATADDR "echo ubuntu | sudo -S grep 'Static interfaces' /etc/network/interfaces"`
+    EDITED=`sshpass -p ubuntu $SSHCMD -t ubuntu@$HOST "echo ubuntu | sudo -S grep 'Static interfaces' /etc/network/interfaces"`
     echo "EDITED = $EDITED"
     if [[ "$EDITED" =~ "Static interfaces" ]]; then
-	echo "interfaces file appears adjusted already"
-	exit 0
+        echo "interfaces file appears adjusted already"
+        exit 0
     else
-	echo "copy interfaces file fragment..."
-	echo sshpass -p ubuntu $SCPCMD -p "vm-eth.txt" "ubuntu@$NATADDR:/tmp"
-	sshpass -p ubuntu $SCPCMD -p "vm-eth.txt" "ubuntu@$NATADDR:/tmp"
-	echo "add the network definitions"
-	# can't simply cat file >> file when updating the interfaces, due to the permissions
-	# rename does work to atomically replace the file
-	sshpass -p ubuntu $SSHCMD -t ubuntu@$NATADDR "echo ubuntu | sudo -S cp -p /etc/network/interfaces /etc/network/interfaces.orig"
-	sshpass -p ubuntu $SSHCMD -t ubuntu@$NATADDR "echo ubuntu | sudo -S cat /etc/network/interfaces /tmp/vm-eth.txt > /tmp/combined.txt"
-	sshpass -p ubuntu $SSHCMD -t ubuntu@$NATADDR "echo ubuntu | sudo -S mv /tmp/combined.txt /etc/network/interfaces"
-	echo "restart networking"
-	echo sshpass -p ubuntu $SSHCMD -t ubuntu@$NATADDR "echo ubuntu | sudo -S service networking restart"
-	sshpass -p ubuntu $SSHCMD -t ubuntu@$NATADDR "echo ubuntu | sudo -S service networking restart"
+        echo "copy interfaces file fragment..."
+        sshpass -p ubuntu $SCPCMD -p "vm-eth.txt" "ubuntu@$HOST:/tmp"
+        echo "add the network definitions"
+        # can't simply cat file >> file when updating the interfaces, due to the permissions
+        # rename does work to atomically replace the file
+        sshpass -p ubuntu $SSHCMD -t ubuntu@$HOST "echo ubuntu | sudo -S cp -p /etc/network/interfaces /etc/network/interfaces.orig"
+        sshpass -p ubuntu $SSHCMD -t ubuntu@$HOST "echo ubuntu | sudo -S cat /etc/network/interfaces /tmp/vm-eth.txt > /tmp/combined.txt"
+        sshpass -p ubuntu $SSHCMD -t ubuntu@$HOST "echo ubuntu | sudo -S mv /tmp/combined.txt /etc/network/interfaces"
+        echo "restart networking"
+        sshpass -p ubuntu $SSHCMD -t ubuntu@$HOST "echo ubuntu | sudo -S service networking restart"
     fi
 fi
