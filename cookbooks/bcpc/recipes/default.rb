b9049114c69c7444241c7869bb7f7a9d10558df4
Justin Erenkrantz <justin@erenkrantz.com>
2014-08-20 12:58:32 +0100

    Backwards compatible syntax change to support Chef 11.

--- cookbooks/bcpc/recipes/default.rb
+++ cookbooks/bcpc/recipes/default.rb
@@ -19,7 +19,7 @@
 
 require 'ipaddr'
 
-node.set['bcpc']['management']['ip'] = node['network']['interfaces'][node['bcpc']['management']['interface']]['addresses'].select { |k, v| v['family'] == "inet" and k != node['bcpc']['management']['vip'] }[0].first
+node.set['bcpc']['management']['ip'] = node['network']['interfaces'][node['bcpc']['management']['interface']]['addresses'].select { |k, v| v['family'] == "inet" and k != node['bcpc']['management']['vip'] }.first[0]
 
 mgmt_bitlen = (node['bcpc']['management']['cidr'].match /\d+\.\d+\.\d+\.\d+\/(\d+)/)[1].to_i
 mgmt_hostaddr = IPAddr.new(node['bcpc']['management']['ip'])<<mgmt_bitlen>>mgmt_bitlen
