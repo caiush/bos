50c6ea6594bfc9d278d5126794442a2157df02c7
Chris Morgan <cmorgan2@bloomberg.net>
2014-12-15 12:48:38 -0500

    protect against ceph not being configured yet

--- cookbooks/bcpc/providers/cephconfig.rb
+++ cookbooks/bcpc/providers/cephconfig.rb
@@ -22,29 +22,33 @@ def whyrun_supported?
 end
 
 action :set do
-  Dir.chdir(@new_resource.path)
-  f = Dir.glob((@new_resource.target) +".asok")
-  f.each do |asok|
-    cmd = Mixlib::ShellOut.new("ceph daemon #{@new_resource.path}/#{asok} config get  #{@new_resource.name}").run_command
-    m = /\s*\{ \"#{@new_resource.name}\"\: \"(.*)\"\}/.match(cmd.stdout)
-    if m
-      if m[1] != @new_resource.value
-        converge_by("setting ceph config") do
-          set_cmd = Mixlib::ShellOut.new("ceph daemon #{@new_resource.path}/#{asok} config set #{@new_resource.name} #{@new_resource.value}").run_command
-          if set_cmd.stdout.include?("\"success\"")
-            Chef::Log.info("Ceph target \"#{asok}\" set #{new_resource.name}:#{new_resource.value}")     
-          else
-            Chef::Log.error("Ceph target \"#{asok}\" unable to set  #{new_resource.name}:#{new_resource.value}: #{seet_cmd.stdout}")  
-            raise "Ceph target \"#{asok}\" unable to set  #{new_resource.name}:#{new_resource.value}: #{seet_cmd.stdout}"
+  if Dir.exists?(@new_resource.path)
+    Dir.chdir(@new_resource.path)
+    f = Dir.glob((@new_resource.target) +".asok")
+    f.each do |asok|
+      cmd = Mixlib::ShellOut.new("ceph daemon #{@new_resource.path}/#{asok} config get  #{@new_resource.name}").run_command
+      m = /\s*\{ \"#{@new_resource.name}\"\: \"(.*)\"\}/.match(cmd.stdout)
+      if m
+        if m[1] != @new_resource.value
+          converge_by("setting ceph config") do
+            set_cmd = Mixlib::ShellOut.new("ceph daemon #{@new_resource.path}/#{asok} config set #{@new_resource.name} #{@new_resource.value}").run_command
+            if set_cmd.stdout.include?("\"success\"")
+              Chef::Log.info("Ceph target \"#{asok}\" set #{new_resource.name}:#{new_resource.value}")     
+            else
+              Chef::Log.error("Ceph target \"#{asok}\" unable to set  #{new_resource.name}:#{new_resource.value}: #{seet_cmd.stdout}")  
+              raise "Ceph target \"#{asok}\" unable to set  #{new_resource.name}:#{new_resource.value}: #{seet_cmd.stdout}"
+            end
           end
+        else
+          Chef::Log.info("Ceph target \"#{asok}\" already set #{new_resource.name}:#{new_resource.value}")     
         end
-      else
-        Chef::Log.info("Ceph target \"#{asok}\" already set #{new_resource.name}:#{new_resource.value}")     
+      else      
+        Chef::Log.error("Ceph target \"#{@new_resource.path}/#{asok}\" doesn't have the config value #{new_resource.name}")
+        raise "Ceph target \"#{@new_resource.path}/#{asok}\" doesn't have the config value #{new_resource.name}"
       end
-    else      
-      Chef::Log.error("Ceph target \"#{@new_resource.path}/#{asok}\" doesn't have the config value #{new_resource.name}")
-      raise "Ceph target \"#{@new_resource.path}/#{asok}\" doesn't have the config value #{new_resource.name}"
     end
+  else
+    Chef::Log.info("Ceph directory \"#{@new_resource.path}\" doesn't exist!")
   end
 end
 
